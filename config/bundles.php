21bf7e522356d4f08ebd434f8a4866023539df01
Micha Alt <mail@lean-stack.de>
2017-09-11 21:55:42 +0200

    flex: installed webprofiler
        composer req webprofiler
    
    Comes with twig-bundle ...

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
 ];
