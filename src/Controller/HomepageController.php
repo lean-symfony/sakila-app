632d2fdb9c0585a39660242ddfca387a79686d95
Micha Alt <mail@lean-stack.de>
2017-09-11 22:20:02 +0200

    Annotation routing configured and used.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/", name="homepage")
+     */
     public function index()
     {
         return $this->render('home/index.html.twig');
